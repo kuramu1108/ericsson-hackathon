cf32ca90b9bad48e817787849de63deaffdda37c
kuramu1108 <colagarychen@gmail.com>
2017-03-16 22:42:10 +1100

    navbar and fire simulation

--- index.js
+++ index.js
@@ -16,19 +16,16 @@ lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
 
 lights[ 0 ].position.set( 0, 200, 0 );
-lights[ 1 ].position.set( 100, 200, 100 );
+lights[ 1 ].position.set( 0, 0, 100 );
 lights[ 2 ].position.set( - 100, - 200, - 100 );
 
 scene.add( lights[ 0 ] );
 scene.add( lights[ 1 ] );
 scene.add( lights[ 2 ] );
 
-function contains (variable, arr) {
-    arr.forEach(function (ar, index) {
-        if (variable == ar)
-            return true;
-    });
-}
+var exits = [
+    [0, 3],[11, 3]
+];
 
 var boundaries = [    
     [11, 0],
@@ -49,6 +46,13 @@ var rooms = [
     [3,0],[8,0],[5, 2]
 ];
 
+function belongsTo(x, y, arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (x == arr[i][0] && y == arr[i][1])
+            return true;
+    }
+}
+
 function isBoundary(x, y) {
     for (var i = 0; i < boundaries.length; i++) {
         if (x == boundaries[i][0] && y == boundaries[i][1])
@@ -68,20 +72,20 @@ for (var x = 0; x < 12; x++) {
             var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
             
-            if ( (x == 0 && y == 3) || (x == 11 && y == 3)) {
+            if ( belongsTo(x, y, exits)) {
                 var material = new THREE.MeshPhongMaterial( {
                     color: 0x00ff00,
                     emissive: 0x072534,
                     shading: THREE.FlatShading
                 } );
                 var cube = new THREE.Mesh( geometry, material );
-            } else if (isBoundary(x, y)) {
+            } else if ( belongsTo(x, y, boundaries)) {
                 var material = new THREE.MeshPhongMaterial( {
                     color: 0x808080,
                     shading: THREE.FlatShading
                 } );
                 var cube = new THREE.Mesh( geometry, material );
-            } else if (isRoom(x, y)) {
+            } else if ( belongsTo(x, y, rooms)) {
                 var material = new THREE.MeshPhongMaterial( {
                     color: 0x0000ff,
                 } );
@@ -106,30 +110,72 @@ function render() {
 	renderer.render( scene, camera );
 }
 
-var counter = 0;
+
+// pathways
+var pathCounter = 0;
 var path = [
     [3, 1], [2, 1], [1, 1], [0, 1], [0, 2]
+];
+
+var alternatePathCounter = 0;
+var alternatePath = [
+    [3, 1],[4, 1],[5,1],[6,1],[7,1],[8,1],[9,1],[10,1],[11,1],[11,2]
 ];
 
 var pathway = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshPhongMaterial({
-        color: 0xff0000,
+        color: 0xffff00,
     })
 );
 
+// hazardous area block
+var fireOn = false;
+var fire = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshPhongMaterial({
+        color: 0xff0000
+    })
+);
+
+// render exit route function
 function showPathWay() {
     scene.remove(pathway);
-    pathway.position.x = path[Math.floor(counter)][0] - 6;
-    pathway.position.y = path[Math.floor(counter)][1] - 2;
-    scene.add(pathway);
-    
-    if (counter < path.length-.1)
-        counter+=.1
-    else
-        counter = 0;
+    if (!fireOn) {
+        pathway.position.x = path[Math.floor(pathCounter)][0] - 6;
+        pathway.position.y = path[Math.floor(pathCounter)][1] - 2;
+        scene.add(pathway);
+        
+        if (pathCounter < path.length-.1)
+            pathCounter+=.1
+        else
+            pathCounter = 0;
+    } else {
+        pathway.position.x = alternatePath[Math.floor(alternatePathCounter)][0] - 6;
+        pathway.position.y = alternatePath[Math.floor(alternatePathCounter)][1] - 2;
+        scene.add(pathway);
+        
+        if (alternatePathCounter < alternatePath.length-.1)
+            alternatePathCounter+=.1
+        else
+            alternatePathCounter = 0;
+    }
 }
 
+// event simulation
+function simulateFire() {
+    if (fireOn) {
+        scene.remove(fire);
+    } else {
+        fire.position.x = 1 - 6;
+        fire.position.y = 1 - 2;
+        scene.add(fire);
+    }
+    fireOn = !fireOn;
+}
+
+
+// ajax data requesting
 function requestDangerBlock() {
     $.ajax({
         url: 'http://localhost:5000/',
